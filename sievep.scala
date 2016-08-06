13900655f9e3f20322b546ef936367217a26ae68
Milindu Sanoj Kumarage <agentmilindu@gmail.com>
2016-08-06 14:10:09 +0530

    Some formatting and improvements

--- sievep.scala
+++ sievep.scala
@@ -5,10 +5,11 @@ object Sieve{
 
  def main(args: Array[ String ]) = {		
 
-  assert (args.length == 1, "must have one argument")
+  assert (args.length == 2, "must have two arguments. number of primes required, number of threads to run")
 
   val t0 = java.lang.System.currentTimeMillis()
   val N = args(0).toInt // number of primes required
+  val T = args(1).toInt // number of threads to run
   val primes = new AtomicIntegerArray(N) // will hold the primes
 
   primes.set(0,2)
@@ -16,14 +17,13 @@ object Sieve{
   var nextSlot = new AtomicInteger(1) // next free slot in primes
   var next = new AtomicInteger(3) // next candidate prime to consider
   
-  var e1 = new Eratosthenes(next, nextSlot, N, primes)
-  var e2 = new Eratosthenes(next, nextSlot, N, primes)
+  for( a <- 1 until T){
 
-  e1.start
-  e2.start
+	var e = new Eratosthenes(next, nextSlot, N, primes)
+	e.start
+	e.join
 
-  e1.join
-  e2.join
+  }
 
   println (primes.get(primes.length()-1))
   println ("Time taken: "+(java.lang.System.currentTimeMillis()-t0))
@@ -34,28 +34,30 @@ object Sieve{
  class Eratosthenes(var next: AtomicInteger, var nextSlot: AtomicInteger, var N: Int, var primes: AtomicIntegerArray) extends Thread{
 
   override def run() = {		
-  primes.synchronized {
-  while(nextSlot.get() < N){
+  
+   while(nextSlot.get() < N){
    
    // Test if next is prime;
    // invariant : next is coprime with primes[0..i ) && p = primes(i)
    
-   var i = 0; var p = primes.get(i)
+    var i = 0; var p = primes.get(i)
    
-    while(p*p<=next.get() && next.get()%p != 0){ i += 1; p = primes.get(i) }
+    primes.synchronized {
+
+     while(p*p<=next.get() && next.get()%p != 0){ i += 1; p = primes.get(i) }
 
      if(p*p>next.get()){ // next is prime
 
-      primes.set( nextSlot.get() , next.get()); 
+      primes.set( nextSlot.get(), next.get()); 
       nextSlot.getAndIncrement()
 
      }
 
-    next.getAndIncrement();
+     next.getAndIncrement();
 
+    }
    }
-}
- } 
-}
+  } 
+ }
 }
 
